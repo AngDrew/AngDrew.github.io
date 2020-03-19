d152452795b131ac4d5616f328aa8afda927f1f8
AngDrew <andrewanggada@gmail.com>
2020-03-19 23:29:19 +0700

    fixing wrong equations.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -11,7 +11,7 @@ const RESOURCES = {
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "5a6695dbe971714f2106dddddf4a2260",
-"main.dart.js": "2bd50607f0f0c550c67b43e398556257"
+"main.dart.js": "bcd74d4308d0c234609acf1df3292a29"
 };
 
 self.addEventListener('activate', function (event) {
